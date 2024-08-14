1e8adbb3fddec15e2c59f931665cb81127336101
HoutanTV <houtantv@gmail.com>
2024-08-14 11:51:04 +0330

    a new line commited

--- session6.py
+++ session6.py
@@ -2,11 +2,6 @@ print('hello world!')
 
 def sum(a, b):
     return a +b
-# start
-#100 line code
-#end
 
 
-def rainbow():
-    # ---
-    return 'there is an error before this'
+print('i am a new line')
